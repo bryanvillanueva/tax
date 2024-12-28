d56e1c11b318290342bec1bde698ac6a58807a08
Bryan Villanueva <vbryan0803@gmail.com>
2024-12-29 10:43:25 +1100

    Archivos para deploy y conexion al server

--- src/components/Login.js
+++ src/components/Login.js
@@ -20,7 +20,7 @@ const Login = () => {
     }
 
     try {
-      const response = await axios.post('http://localhost:5000/login', {
+      const response = await axios.post('https://taxbackend-production.up.railway.app/login', {
         email,
         password,
       });
